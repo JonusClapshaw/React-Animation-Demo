2ea15dd177daeec7e4cfa322fca4b9b723ba9e00
Jonus <joclapshaw@gmail.com>
2026-05-28 12:46:00 -0700

    Basic set for showcase

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+import { motion } from "motion/react"
+
+import Header from './components/Header'
+import Box from './components/Simple-Box-Roll'
+
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <Box />
     </>
   )
 }
